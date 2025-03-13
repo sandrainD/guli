45dfcaff102b960eb80485b284a0e92db3536176
sandrain <ltsttl33@gmail.com>
2025-03-13 15:48:09 +0800

    bugfix

--- guli-seckill/src/main/java/com/idea/guli/guliseckill/config/SeckillSentinelConfig.java
+++ guli-seckill/src/main/java/com/idea/guli/guliseckill/config/SeckillSentinelConfig.java
@@ -4,8 +4,8 @@ import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
 import com.alibaba.csp.sentinel.adapter.servlet.callback.WebCallbackManager;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
-import com.atguigu.common.exception.BizCodeEnume;
-import com.atguigu.common.utils.R;
+import com.idea.common.exception.BizCodeEnume;
+import com.idea.common.utils.R;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,7 +19,7 @@ public class SeckillSentinelConfig {
         WebCallbackManager.setUrlBlockHandler(new UrlBlockHandler() {
             @Override
             public void blocked(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws IOException {
-                R error = R.error(BizCodeEnume.TOO_MANY_REQUEST.getCode(), BizCodeEnume.TOO_MANY_REQUEST.getMsg());
+                R error = R.error(BizCodeEnume.TO_MANY_REQUEST.getCode(), BizCodeEnume.TO_MANY_REQUEST.getMsg());
                 httpServletResponse.setCharacterEncoding("UTF-8");
                 httpServletResponse.setContentType("application/json");
                 httpServletResponse.getWriter().write(JSON.toJSONString(error));
