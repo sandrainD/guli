1f4b0c1907b9f1be06986b60d15ce7517372464f
sandrain <ltsttl33@gmail.com>
2025-03-14 14:23:05 +0800

    sentinel熔断降级 sleuth链路追踪 zipkin可视化观察

--- guli-seckill/src/main/java/com/idea/guli/guliseckill/config/SeckillSentinelConfig.java
+++ guli-seckill/src/main/java/com/idea/guli/guliseckill/config/SeckillSentinelConfig.java
@@ -1,29 +1,29 @@
 package com.idea.guli.guliseckill.config;
 
-import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
-import com.alibaba.csp.sentinel.adapter.servlet.callback.WebCallbackManager;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
-import com.alibaba.fastjson.JSON;
-import com.idea.common.exception.BizCodeEnume;
-import com.idea.common.utils.R;
-import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+//import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
+//import com.alibaba.csp.sentinel.adapter.servlet.callback.WebCallbackManager;
+//import com.alibaba.csp.sentinel.slots.block.BlockException;
+//import com.alibaba.fastjson.JSON;
+//import com.idea.common.exception.BizCodeEnume;
+//import com.idea.common.utils.R;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
 
 //@Configuration
-public class SeckillSentinelConfig {
-
-    public SeckillSentinelConfig(){
-        WebCallbackManager.setUrlBlockHandler(new UrlBlockHandler() {
-            @Override
-            public void blocked(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws IOException {
-                R error = R.error(BizCodeEnume.TO_MANY_REQUEST.getCode(), BizCodeEnume.TO_MANY_REQUEST.getMsg());
-                httpServletResponse.setCharacterEncoding("UTF-8");
-                httpServletResponse.setContentType("application/json");
-                httpServletResponse.getWriter().write(JSON.toJSONString(error));
-            }
-        });
-    }
-}
+//public class SeckillSentinelConfig {
+//
+//    public SeckillSentinelConfig(){
+//        WebCallbackManager.setUrlBlockHandler(new UrlBlockHandler() {
+//            @Override
+//            public void blocked(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws IOException {
+//                R error = R.error(BizCodeEnume.TO_MANY_REQUEST.getCode(), BizCodeEnume.TO_MANY_REQUEST.getMsg());
+//                httpServletResponse.setCharacterEncoding("UTF-8");
+//                httpServletResponse.setContentType("application/json");
+//                httpServletResponse.getWriter().write(JSON.toJSONString(error));
+//            }
+//        });
+//    }
+//}
